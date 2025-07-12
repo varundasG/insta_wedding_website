f4617b4fc9446385e3dd1bde32a7a2f3428a3ced
varundasG <varundaskollengode@gmail.com>
2025-07-12 06:15:16 +0530

    e

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/insta_wedding_website/', // replace with your repo name
+  base: '/insta_wedding_website/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
